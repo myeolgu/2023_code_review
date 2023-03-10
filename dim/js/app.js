bc9f1a736204ff0bf88b16eccaacb99621993a08
leejuyeop <juyeop198@etribe.co.kr>
2023-03-10 17:18:49 +0900

    20230310 doitdoit

--- dim/js/app.js
+++ dim/js/app.js
@@ -1,1 +1,29 @@
 const popupEvent = document.querySelector('.button_popup');
+const popupDim = document.querySelector('.popup_dim');
+const popupWrap = document.querySelector('.popup_wrap');
+const popupClose = document.querySelector('.popup_close');
+
+
+popupEvent.addEventListener('click', function(event){
+    popupDim.style.display = "block";
+    document.body.style.overflowY = "hidden";
+    setTimeout(() => {
+        popupWrap.style.display = "block";
+    },100);
+});
+
+popupDim.addEventListener('click', function(event){
+    document.body.style.overflowY = "auto";
+    popupWrap.style.display = "none";
+    setTimeout(() => {
+        popupDim.style.display = "none";
+    },100);
+});
+
+popupClose.addEventListener('click', function(event){
+    document.body.style.overflowY = "auto";
+    popupWrap.style.display = "none";
+    setTimeout(() => {
+        popupDim.style.display = "none";
+    },100);
+});
