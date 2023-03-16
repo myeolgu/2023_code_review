1dd891c85688a046e2a3901995551b9572de5e8b
leejuyeop <juyeop198@etribe.co.kr>
2023-03-16 17:15:58 +0900

    무하하하하

--- publish/js/script.js
+++ publish/js/script.js
@@ -1,13 +1,9 @@
-const menubox = document.querySelectorAll('.gnb_menu li');
+const menubox = document.querySelectorAll('.gnb_menu li [data-id]');
 const dropmenu = document.querySelector('.header_dropbox');
 const header = document.getElementById('header');
 
-console.dir(menubox);
-
 menubox.forEach(e =>
     e.addEventListener("mouseover", () => {
-        // dropmenu.style.display = "block";
-        // header.style.backgroundColor = "white";
         dropmenu.classList.add('active');
         setTimeout(() => {
 
